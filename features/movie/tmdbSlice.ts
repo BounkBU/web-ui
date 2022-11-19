30da1bc3e190b3a895be63cabdd56de9ec625cff
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-20 01:42:35 +0700

    feat: Added redux fetch tmdb movie detail

--- features/movie/tmdbSlice.ts
+++ features/movie/tmdbSlice.ts
@@ -1,14 +1,21 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '../../app/store'
 import client from '../../client'
-import type { ITmdbMovieFilter, TmdbMovieI } from '../../types/tmdb'
+import type {
+  ITmdbMovieDetailFilter,
+  ITmdbMovieFilter,
+  TmdbMovieDetailI,
+  TmdbMovieI,
+} from '../../types/tmdb'
 
 interface ITmdbMovieSlice {
   movies: TmdbMovieI[]
+  movieDetail: TmdbMovieDetailI | null
 }
 
 const initialState: ITmdbMovieSlice = {
   movies: [],
+  movieDetail: null,
 }
 
 export const fetchTmdbMovies = createAsyncThunk(
@@ -19,6 +26,14 @@ export const fetchTmdbMovies = createAsyncThunk(
         page: page,
       },
     })
+    return data
+  },
+)
+
+export const fetchTmdbMovieDetail = createAsyncThunk(
+  'movie/fetchTmdbMovieDetail',
+  async ({ id }: ITmdbMovieDetailFilter) => {
+    const { data } = await client.get<TmdbMovieDetailI>(`/tmdb/movie/${id}`)
     return data
   },
 )
@@ -40,10 +55,17 @@ export const tmdbSlice = createSlice({
       (state: ITmdbMovieSlice, action: PayloadAction<TmdbMovieI[]>) => {
         state.movies = action.payload
       },
-    )
+    ),
+      builder.addCase(
+        fetchTmdbMovieDetail.fulfilled,
+        (state: ITmdbMovieSlice, action: PayloadAction<TmdbMovieDetailI>) => {
+          state.movieDetail = action.payload
+        },
+      )
   },
 })
 
 export const tmdbMoviesState = (state: RootState) => state.tmdb.movies
+export const thdbMovieDetailState = (state: RootState) => state.tmdb.movieDetail
 
 export default tmdbSlice.reducer
