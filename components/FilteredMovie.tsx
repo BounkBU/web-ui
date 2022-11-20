8c828b7834f835104740e5c9e9c4dda129570ea8
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-21 01:36:37 +0700

    feat: Added filtered movie component

---
+++ components/FilteredMovie.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import type { Movie } from '../types/components/movie'
+
+export default function FilteredMovie({
+  movie,
+  isMovie,
+}: {
+  movie: Movie
+  isMovie: boolean
+}) {
+  return (
+    <Link
+      href={`/${isMovie ? 'movie' : 'serie'}/${movie.id}`}
+      className='mt-4 first:mt-0'
+    >
+      <div className='flex items-center border border-zinc-700 rounded-lg cursor-pointer'>
+        <Image
+          src={movie.poster_path}
+          alt=''
+          width={90}
+          height={150}
+          className='rounded-l-lg'
+        />
+        <span className='ml-8 text-xl'>{movie.title}</span>
+      </div>
+    </Link>
+  )
+}
