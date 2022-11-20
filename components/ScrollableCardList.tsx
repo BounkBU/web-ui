511f79c04ca011713ca097093c3f6178c52780d3
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-20 15:25:43 +0700

    feat: Added scrolleble card list props

--- components/ScrollableCardList.tsx
+++ components/ScrollableCardList.tsx
@@ -1,15 +1,14 @@
 import ScrollableCard from './ScrollableCard'
 import Slider from 'react-slick'
 import { sliderSettings } from '../config'
+import type { ScrollableCardListProps } from '../types/components/card'
 
-export default function CardList() {
+export default function ScrollableCardList({ title }: ScrollableCardListProps) {
   const sampleArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
   return (
     <div className='w-[80%] max-w-[900px] mx-auto my-8'>
-      <span className='text-xl font-semibold text-white'>
-        Top picks for you
-      </span>
+      <span className='text-xl font-semibold text-white'>{title}</span>
       <div className='mt-4'>
         <Slider {...sliderSettings}>
           {sampleArray.map((el) => (
