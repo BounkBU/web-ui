c12914608f77280c5272df54d21fb313153cac58
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-11-21 19:56:34 +0700

    feat: Filtered movie search results on search bar

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,15 +1,51 @@
 import { useRouter } from 'next/router'
-import { FormEvent, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { searchResultMoviesState } from '../features/movie/movieSlice'
 import type { SearchbarProps } from '../types/components/searchbar'
+import type { Movie, Result } from '../types/movie'
+import SearchMovieCard from './SearchMovieCard'
 
-export default function Searchbar({ defaultValue, disabled }: SearchbarProps) {
+export default function Searchbar({
+  defaultValue,
+  disabled,
+  isShowResults,
+}: SearchbarProps) {
   const router = useRouter()
+  const searchResultMovies = useAppSelector(searchResultMoviesState)
   const [searchValue, setSearchValue] = useState(defaultValue || '')
+  const [searchResults, setSearchResults] = useState<Movie[]>()
 
   function onSubmitHandler(e: FormEvent) {
     e.preventDefault()
     router.push(`/search?q=${searchValue}`)
   }
+
+  function onSetSearchResultsMovieHandler(
+    results: Result[],
+    searchValue: string,
+  ) {
+    const allMovies = results.map((result: Result) => {
+      return result.movies
+        .filter((movie) => movie.title.toLowerCase().includes(searchValue))
+        .map((movie) => {
+          return {
+            ...movie,
+            isMovie: result.topic.toLowerCase().includes('movie'),
+          }
+        })
+    })
+    const initialSelectedMovies = allMovies.reduce(
+      (accumulator: Movie[], currentValue: Movie[]) =>
+        accumulator.concat(currentValue),
+      [],
+    )
+    setSearchResults(initialSelectedMovies)
+  }
+
+  useEffect(() => {
+    onSetSearchResultsMovieHandler(searchResultMovies, searchValue)
+  }, [searchResultMovies, searchValue])
 
   return (
     <form
@@ -31,7 +67,17 @@ export default function Searchbar({ defaultValue, disabled }: SearchbarProps) {
       >
         SEARCH
       </button>
-      <div className='search-results absolute w-full bg-zinc-700 max-h-96 top-12 rounded overflow-y-auto z-10'></div>
+      {isShowResults && searchValue !== '' && (
+        <div className='search-results absolute w-full bg-zinc-700 max-h-96 top-12 rounded overflow-y-auto z-10'>
+          {searchResults?.map((movie, index) => (
+            <SearchMovieCard
+              key={index}
+              movie={movie}
+              isMovie={movie.isMovie}
+            />
+          ))}
+        </div>
+      )}
     </form>
   )
 }
